a66054ccd7754618724d5c44a95195670099b272
Vishal Sood <vishal.sood@saphetor.com>
2016-04-05 15:19:58 +0200

    polymorphise Xrange

--- include/util.h
+++ include/util.h
@@ -24,40 +24,69 @@ ContainerOut collect(ContainerIn xs, ContainerOut ys) {
     return ys;
 }
 
+template< typename T >
+const T id(const T& t) { return t;}
+
+template< typename T >
+T id(T& t) { return t;}
+
+template<UnaryOp F, typename T>
 class Xrange {
 public:
 Xrange(int32_t low, int32_t high):
-    _low((high - low) >= 0 ? low : high),
-        _high((high - low) >= 0 ? high : low),
-        _delta((high - low) >= 0 ? 1 : -1) {}
+  _low((high - low) >= 0 ? low : high),
+    _high((high - low) >= 0 ? high : low),
+    _delta((high - low) >= 0 ? 1 : -1),
+    _apply(id<int32_t>) {}
 
 Xrange(int32_t low, int32_t high, int32_t delta):
-    _low(delta*(high - low) >= 0 ? low : high),
-        _high(delta*(high - low) >= 0 ? high : low ),
-        _delta(delta) {}
+  _low(delta*(high - low) >= 0 ? low : high),
+    _high(delta*(high - low) >= 0 ? high : low ),
+    _delta(delta),
+    _apply(id<int32_t>) {}
+
+Xrange(F f, int32_t low, int32_t high):
+  _low((high - low) >= 0 ? low : high),
+    _high((high - low) >= 0 ? high : low),
+    _delta((high - low) >= 0 ? 1 : -1),
+    _apply(f) {}
+
+Xrange(F f, int32_t low, int32_t high, int32_t delta):
+  _low(delta*(high - low) >= 0 ? low : high),
+    _high(delta*(high - low) >= 0 ? high : low ),
+    _delta(delta),
+    _apply(f) {}
 
     class Iterator {
     public:
     Iterator(uint32_t pos, int32_t delta) : _pos(pos), _delta(delta) {}
-        uint32_t pos() const { return _pos; }
-        int32_t delta() const {return _delta; }
-        int32_t operator*() const { return _pos; }
-        Iterator& operator++() {
-            _pos += _delta;
-            return *this;
-        }
+      using iterator_category = std::forward_iterator_tag;
+      using value_type = T;
+      using difference_type = int32_t;
+      using pointer = T*;
+      using reference = T&;
+      uint32_t pos() const { return _pos; }
+      int32_t delta() const {return _delta; }
+      const T& operator*() const { return _current; }
+      Iterator& operator++() {
+        _pos += _delta;
+        _current = _apply(_pos);
+        return *this;
+      }
 
-        bool operator!=(const Iterator& that) {
-            return _pos != that.pos() or _delta != that.delta();
-        }
+      bool operator!=(const Iterator& that) {
+        return _pos != that.pos() or _delta != that.delta();
+      }
 
-        bool operator==(const Iterator& that) {
-            return _pos == that.pos() and _delta == that.delta();
-        }
+      bool operator==(const Iterator& that) {
+        return _pos == that.pos() and _delta == that.delta();
+      }
 
     private:
         uint32_t _pos = 0;
         int32_t _delta = 1;
+        const static F _apply;
+        T current = _apply(_pos);
     };
 
 
