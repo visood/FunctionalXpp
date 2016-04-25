a731720f7df386663bd1aa75f0188337a5f99346
Vishal Sood <vishal.sood@saphetor.com>
2016-04-25 12:39:26 +0200

    iterate a Relational Database (Sim) with an iterator

---
+++ include/RelationalDatabaseSim.h
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <tuple>
+#include "catch.hpp"
+#include <string>
+#include <limits>
+
+template<typename T>
+struct DType {};
+
+namespace DataType {
+    const DType<double> Double;
+    const DType<std::string> String;
+    const DType<int> Integer;
+
+    template<typename T, typename S>
+    T convert(const S& data) {
+        std::stringstream i;
+        i << data;
+        T x;
+        if (!(i >> x)) throw std::invalid_argument("invalid data");
+        return x;
+    }
+    template<typename T>
+    T convert(const std::string& data) {
+        std::stringstream i;
+        i << data;
+        T x;
+        if (!(i >> x)) throw std::invalid_argument("invalid data string: " + data);
+        return x;
+    }
+
+    template<typename T, typename S>
+    T getValue(T& t, const S& s) {
+      t = convert<T, S>(s);
+      return t;
+    }
+
+};
+
+#if 0
+template<std::size_t>
+class DataStr {
+ public:
+ DataStr(int n) : _size(n) {}
+
+  template<typename T>
+ int insert(const T& x) {
+    std::istringstream i(x);
+    std::string x;
+    if ()
+  }
+};
+#endif
+
+class StrRdbRow {
+  //using DataStr = std::vector<std::string>;
+
+ public:
+StrRdbRow(std::vector<std::string> row) : _data(row) {}
+
+  uint nfields() const { return (uint) _data.size();}
+  uint size() const { return (uint) _data.size();}
+  void check_index(uint index) const {
+    if (index > (uint) _data.size())
+      throw std::invalid_argument("requested index " +
+                                  DataType::convert<std::string, uint>(index) +
+                                  " is larger than row size " +
+                                  DataType::convert<std::string, uint>((uint) _data.size()));
+  }
+
+  std::string getString(uint index) const { check_index(index); return _data[index - 1];}
+  double getDouble(uint index) const { check_index(index); return DataType::convert<double, std::string>(_data[index - 1]);}
+  int getInt(uint index) const { check_index(index); return  DataType::convert<int, std::string>(_data[index - 1]);}
+
+  void print() {
+    for (auto f: _data)
+      std::cout << f << ", ";
+    std::cout << std::endl;
+  }
+
+ private:
+  std::vector<std::string> _data;
+};
+
+class StrRowRdbTable {
+ public:
+StrRowRdbTable(uint ncol) : _ncol(ncol), _nrow(0) {}
+StrRowRdbTable(uint ncol, const std::vector< std::vector<std::string> >& table) : _ncol(ncol), _nrow((uint) table.size()) {
+    for (auto row: table) {
+      insert(row);
+    }
+  }
+
+  template<typename Sized>
+  void check_row_size(const Sized& v) const {
+    if (v.size() != _ncol)
+      throw std::invalid_argument("A row of " + DataType::convert<std::string, int>(_ncol) +
+                                  " cannot be extracted from a vector of length " +
+                                  DataType::convert<std::string, int>(v.size()));
+  }
+  uint size() const { return (uint) _data.size();}
+  uint position() const { return _current;}
+  void insert(std::vector<std::string> row) {
+    push_back( StrRdbRow(row));
+  }
+  void insert(StrRdbRow row) {
+    push_back( StrRdbRow(row));
+  }
+  void push_back(StrRdbRow row) {
+    check_row_size(row);
+    _data.push_back(row);
+  }
+
+  StrRowRdbTable* next()  {
+    if (not _started) {
+      _started = true;
+    } else {
+      _current += 1;
+    }
+    if (_current >= _data.size()) { return nullptr;}
+    return this;
+  }
+
+  std::string getString(uint index) const {  return _data[_current].getString(index);}
+  double getDouble(uint index) const {  return _data[_current].getDouble(index);}
+  int getInt(uint index) const {  return  _data[_current].getInt(index);}
+
+  void printCurrent() {
+    _data[_current].print();
+  }
+ private:
+  uint _ncol = 0;
+  uint _nrow = 0;
+  std::vector< StrRdbRow > _data;
+  uint _current = 0;
+  bool _started = false;
+};
+
+
+template<typename ResType>
+class IterableRDB {
+public:
+IterableRDB(ResType* res) : _start(res) {}
+
+  class Iterator {
+  public:
+  Iterator(ResType* res, int p) : _current(res), _position(p), _started(false) {}
+
+    using self_type = Iterator;
+    using self_reference = Iterator&;
+    using iterator_category = std::input_iterator_tag;
+    using value_type = ResType*;
+    using pointer = value_type const*;
+    using reference = value_type const&;
+    using difference_type = int32_t;
+
+    uint position() const {return _position;}
+    reference operator*() const { return _current;}
+    pointer operator->() const {return &_current;}
+    self_reference operator++() {
+      if (not _started) {
+        _started = true;
+      } else {
+        _position += 1;  
+      }
+      _current = _current->next();
+      if (not _current) {
+        _position = std::numeric_limits<int>::max();
+      }
+      return *this;
+    }
+    bool operator !=(const Iterator& that) const {
+      return _position != that.position();
+    }
+  private:
+    ResType* _current;
+    uint _position = 0;
+    bool _started = false;
+  };
+
+  const Iterator begin() const {
+    return Iterator(_start, 0);
+  }
+
+  const Iterator end() const {
+    return Iterator(nullptr, std::numeric_limits<int>::max());
+  }
+
+private:
+  ResType* _start = nullptr;
+};
--- test/main.cpp
+++ test/main.cpp
@@ -3,15 +3,129 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 //#include "FunctionalXpp.h"
-#include "DatabaseTable.h"
+#include "RelationalDatabaseSim.h"
 
+
+std::string wordyOneToNine(uint x) {
+  if (x > 9) throw std::invalid_argument("wordyOneToNine cannot wordify number larger than 9");
+  if (x == 0) throw std::invalid_argument("wordyOneToNine cannot wordify 0");
+  switch (x) {
+  case 9:
+    return "nine";
+  case 8:
+    return "eight";
+  case 7:
+    return "seven";
+  case 6:
+    return "six";
+  case 5:
+    return "five";
+  case 4:
+    return "four";
+  case 3:
+    return "three";
+  case 2:
+    return "two";
+  case 1:
+    return "one";
+  case 0:
+    return "";
+  }
+  return "";
+}
+
+std::string wordyInteger(uint x) {
+  if (x > 1000) throw std::invalid_argument("wordInteger cannot wordify numbers that are larger than 1000");
+  std::string wordy;
+  if (x > 100) wordy = wordyOneToNine(x/100) + " hundred";
+  if (x > 100 and x % 100 > 0)  wordy += " and ";
+  x = x % 100;
+  switch (x / 10) {
+  case (9):
+    wordy += "ninety";
+    break;
+  case (8):
+    wordy += "eighty";
+    break;
+  case (7):
+    wordy += "seventy";
+    break;
+  case (6):
+    wordy += "sixty";
+    break;
+  case (5):
+    wordy += "fifty";
+    break;
+  case (4):
+    wordy += "forty";
+    break;
+  case (3):
+    wordy += "thirty";
+    break;
+  case (2):
+    wordy += "twenty";
+    break;
+  case (1):
+    switch (x % 10) {
+    case (9):
+      wordy += "nineteen";
+      return wordy;
+    case (8):
+      wordy += "eighteen";
+      return wordy;
+    case (7):
+      wordy += "seventeen";
+      return wordy;
+    case (6):
+      wordy += "sixteen";
+      return wordy;
+    case (5):
+      wordy += "fifteen";
+      return wordy;
+    case (4):
+      wordy += "fourteen";
+      return wordy;
+    case (3):
+      wordy += "thirteen";
+      return wordy;
+    case (2):
+      wordy += "twelve";
+      return wordy;
+    case (1):
+      wordy += "eleven";
+      return wordy;
+    }
+  case (0):
+    break;
+  }
+  if (x % 10 == 0) return wordy;
+  if (x > 10 ) wordy += " ";
+  wordy += wordyOneToNine( x % 10 );
+  return wordy;
+}
+
+TEST_CASE("numbers can be converted to words", "[Wordify]") {
+  std::cout <<  wordyInteger(987) << std::endl;
+  REQUIRE( wordyInteger(987) == "nine hundred and eighty seven");
+  std::cout << wordyInteger(570) << std::endl;
+  REQUIRE( wordyInteger(570) == "five hundred and seventy");
+  std::cout << wordyInteger(507) << std::endl;
+  REQUIRE( wordyInteger(507) == "five hundred and seven");
+  std::cout << wordyInteger(500) << std::endl;
+  REQUIRE( wordyInteger(500) == "five hundred");
+  std::cout << wordyInteger(90) << std::endl;
+  REQUIRE( wordyInteger(90) == "ninety");
+  std::cout << wordyInteger(19) << std::endl;
+  REQUIRE( wordyInteger(19) == "nineteen");
+  std::cout << wordyInteger(9) << std::endl;
+  REQUIRE( wordyInteger(9) == "nine");
+}
 
 TEST_CASE ("Tuples can be printed", "[printTuples]") {
     auto t1 = std::make_tuple(1, 0.0f, "hello");
     std::cout << "size of the tuple "
               << std::tuple_size<std::tuple<int, float, char*>>::value << std::endl;
     std::cout << std::get<0> (t1) << ", " << std::get<1>(t1) << ", " << std::get<2>(t1) << std::endl;
-    std::cout << t1 << std::endl;
 }
 
 /*
@@ -21,25 +135,80 @@ TEST_CASE ("DataTypes are available", "[datatypes]") {
     REQUIRE (IntegerType);
 }
 */
-TEST_CASE ("Values of a certain type can be obtained from a string", "[getValue]") {
+TEST_CASE ("Values of a certain type can be obtained from a string", "[DataTypes]") {
     double xdouble;
     int xint;
     std::string xstr;
-    getValue(xdouble, "1.0");
-    getValue(xint, "1");
-    getValue(xstr, "one");
+    DataType::getValue(xdouble, "1.0");
+    DataType::getValue(xint, "1");
+    DataType::getValue(xstr, "one");
     REQUIRE( xdouble == 1.0);
     REQUIRE( xint == 1);
     REQUIRE( xstr == "one");
 
-    double xd = DataType::get<double> ("1.0");
-    int xi = DataType::get<int> ("1");
-    std::string xs = DataType::get<std::string> ("one");
+    double xd = DataType::convert<double> ("1.0");
+    int xi = DataType::convert<int> ("1");
+    std::string xs = DataType::convert<std::string> ("one");
     REQUIRE( xd == 1.0);
     REQUIRE( xi == 1);
     REQUIRE( xs == "one");
 }
 
+TEST_CASE ("Simulate a Relational Database Row", "[RDBsim], [RDBrowSim]") {
+  std::vector<std::string> rowVec {"1.0", "1", "one"};
+  StrRdbRow* row = new StrRdbRow(rowVec);
+  REQUIRE (row->getDouble(1) == 1.0);
+  REQUIRE (row->getInt(2) == 1);
+  REQUIRE (row->getString(3) == "one");
+  delete(row);
+}
 
+TEST_CASE("Simulate a Relational Database Table", "[RDBSim], [RDBtableSim]") {
+  std::vector<std::vector<std::string> > table;
+  for (uint i = 0; i != 100; ++i) {
+    std::vector<std::string> row{ DataType::convert<std::string, double>((double) i),
+        DataType::convert<std::string, uint>(i),
+        wordyInteger(i)};
+    table.push_back(row);
+  }
 
+  StrRowRdbTable* res = new StrRowRdbTable(3, table);
+  REQUIRE( res );
+  REQUIRE( res->size() == (uint) table.size());
+  uint i = 0;
+  while (res->next()) {
+    //res->printCurrent();
+    REQUIRE( res->getDouble(1) == (double) i);
+    REQUIRE( res->getInt(2) == (int) i);
+    REQUIRE( res->getString(3) == wordyInteger(i));
+    i += 1;
+  }
 
+  delete res;
+}
+
+TEST_CASE("Iterator to a RelationalDatabaseSim works", "[RDBSim], [RDBtableSim], [RDBtableSimIterator]") {
+  std::vector<std::vector<std::string> > table;
+  for (uint i = 0; i != 100; ++i) {
+    std::vector<std::string> row{ DataType::convert<std::string, double>((double) i),
+        DataType::convert<std::string, uint>(i),
+        wordyInteger(i)};
+    table.push_back(row);
+  }
+
+  StrRowRdbTable* res = new StrRowRdbTable(3, table);
+  REQUIRE( res );
+  REQUIRE( res->size() == (uint) table.size());
+
+  //std::cout << "sum doubles using an iterator" << std::endl;
+  IterableRDB<StrRowRdbTable> rdbItr(res);
+
+  double sum = std::accumulate(rdbItr.begin(), rdbItr.end(), 0.0,
+                               [] (const double s, const auto row) {
+                                 //std::cout << row->position() << std::endl;
+                                 return s + row->getDouble(1);
+                               });
+
+  //std::cout << "summed doubles " << sum << std::endl;
+  REQUIRE( sum == 4950);
+}
